17c9e25c18f81587fa8489d9959f498060b52264
nakul-3205 <nakulkejriwal079@gmail.com>
2025-08-05 18:49:32 +0530

    database setup completed

---
+++ push_data.py
@@ -0,0 +1,69 @@
+import os
+import json
+import sys
+from dotenv import load_dotenv
+import pandas as pd 
+import pymongo
+import numpy as np
+from scanify_ai.exception.exception import CustomException
+from scanify_ai.logging.log_config import logger
+
+
+load_dotenv()
+
+MONGO_URL=os.getenv('MONGODB_URI')
+import certifi
+ca=certifi.where()
+
+
+class DataExtract():
+    def __init__(self):
+        try:
+            pass
+        except Exception as e :
+            raise CustomException(e,sys)
+    
+    def csv_to_json(self,file_path):
+        try:
+            data=pd.read_csv(file_path)
+            data.reset_index(drop=True,inplace=True)
+            # records=json.loads((data.T.to_json()).values())
+            records = json.loads(data.T.to_json())
+            records = list(records.values())  
+
+            return records
+        except Exception as e :
+            raise CustomException(e,sys)
+    
+    def load_in_mongo(self,records,collection,database):
+        try:
+            self.database=database
+            self.collection=collection
+            self.records=records
+            self.mongo_client=pymongo.MongoClient(MONGO_URL)
+            self.database=self.mongo_client[self.database]
+            self.collection=self.database[self.collection]
+            self.collection.insert_many(self.records)
+            logger.info('sucessfull connect to database')
+
+            return( len(self.records))
+        
+        
+        
+        except Exception as e:
+            raise CustomException(e,sys)
+    
+
+if __name__=='__main__':
+    FILE_PATH="Network_Data/phisingData.csv"
+    DATABASE="SCANIFYAI_DATABASE"
+    COLLECTION="SCANIFY_AI"
+    networkobj=DataExtract()
+    record=networkobj.csv_to_json(file_path=FILE_PATH)
+    no_of_records=networkobj.load_in_mongo(record,DATABASE,COLLECTION)
+    print(no_of_records)
+    
+logger.info('sucessfully loaded the data in mongo')
+
+
+
